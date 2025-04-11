8563f329e826cc95ba38f71dc8b3aee48595befb
Giovanni-Iapichino <giovanniiapichino28@gmail.com>
2025-04-11 09:47:06 +0200

    fix-form-html

--- biglietto-del-treno.js
+++ biglietto-del-treno.js
@@ -1,7 +1,7 @@
-//todo Scrivere un programma che chieda all’utente:
+//# Scrivere un programma che chieda all’utente:
 //Il numero di chilometri da percorrere
 //Età del passeggero
-//todo Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
+//# Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio, secondo le seguenti regole:
 //* - il prezzo del biglietto è definito in base ai km (0.21 € al km)
 //* - va applicato uno sconto del 20% per i minorenni
 //* - va applicato uno sconto del 40% per gli over 65.
@@ -13,7 +13,7 @@
 //? Ora che la logica è funzionante in pagina, possiamo andare a dedicarci allo stile, raffinando la parte di HTML e CSS in modo da renderla esteticamente gradevole.
 //Nota: Se non vi sentite particolarmente creativi, questa potrebbe essere un’implementazione da seguire per il secondo milestone. Potete scegliere di implementare una soluzione completamente diversa oppure simile, ma in ogni caso cercate di farla vostra.
 
-//todo FORM ELEMENT
+//# FORM ELEMENT
 const inputNameEl = document.querySelector("#full-name");
 const inputKmEl = document.querySelector("#km-to-travel");
 const inputAge = document.querySelector("#age");
@@ -28,13 +28,13 @@ console.log("Nome e Cognome: ", inputNameEl.value);
 console.log("Km da percorrere: ", inputKmEl.value);
 console.log("Età :", inputAge.value);
 
-//todo CARD ELEMENT
+//# CARD ELEMENT
 const nameCardEl = document.querySelector("#name-card .card-body");
 const ageCardEl = document.querySelector("#age-card .card-body");
 const priceCardEl = document.querySelector("#price-card .card-body");
 const allCardEl = document.querySelector("#all-card");
 
-//todo SUBMIT FORM
+//# SUBMIT FORM
 generateTicketFormEl.addEventListener("submit", function (event) {
   event.preventDefault();
   const fullName = inputNameEl.value;
@@ -42,12 +42,13 @@ generateTicketFormEl.addEventListener("submit", function (event) {
   const age = inputAge.value;
   const ticketFullPrice = priceForKm * kmToTravel;
 
-  //todo CONTROLLO DATI
+  //# CONTROLLO DATI
   let hasError = false;
   if (!fullName || !isNaN(fullName)) hasError = true;
   if (!kmToTravel || isNaN(kmToTravel)) hasError = true;
   if (!age || isNaN(age)) hasError = true;
 
+  //# LOGICA PROGRAMMA
   if (hasError) {
     generateTicketFormEl.classList.add("d-none");
     errorMessage.classList.remove("d-none");
@@ -70,6 +71,7 @@ generateTicketFormEl.addEventListener("submit", function (event) {
     ticketFinalPrice = ticketFullPrice.toFixed(2);
   }
 
+  //# STAMPA DEI DATI IN CONSOLE
   console.log("Biglietto generato");
   console.log("Nome: ", fullName);
   console.log("Fascia d'età: ", age);
@@ -81,6 +83,7 @@ generateTicketFormEl.addEventListener("submit", function (event) {
   priceCardEl.innerText = `${"Prezzo finale: €"} ${ticketFinalPrice}`;
 });
 
+//# SVUOTA IL FORM
 deleteButton.addEventListener("click", function () {
   generateTicketFormEl.reset();
   nameCardEl.innerText = "";
